df63361812a86607bead5b6a0c405a1bf13d8e76
nimbus9030 <nimbus9030@gmail.com>
2020-10-29 17:29:59 +0900

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,50 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">新規作成</div>
+                
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form class="form-horizontal" method="POST" action="">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="title" class="col-md-4 control-label">タイトル</label>
+                            <div class="">
+                                <input id="title" type="text" class="form-control" name="title" value="{{ old('title') }}" required autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="title" class="col-md-4 control-label">本文</label>
+                            <div class="">
+                                <textarea id="content" name="content" class="form-control" cols="50" rows="10" required></textarea>
+                            </div>
+                        
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">新規作成</div>
@@ -36,6 +79,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 </div>
 @endsection
