7654cea47535d930b9397ddb52ced7c108de5681
nimbus9030 <nimbus9030@gmail.com>
2020-10-30 11:24:34 +0900

    Update bbs.blade.php

--- resources/views/bbs.blade.php
+++ resources/views/bbs.blade.php
@@ -48,7 +48,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-            <!-- {{ $posts }} -->
+            <!-- Posts -->
             <!-- Pager -->
             <div class="clearfix">
             </div>
